b00372d96d972e8b705c648f2e1788e96084d2c9
Jonathan Wolper <jpwolper@gmail.com>
2018-07-02 15:42:13 -0400

    Revert "attempted to hook up Ember Data"
    This reverts commit c8a75e58a0fc0a24fc88afa572829a455a22f386.

--- app/routes/contact-list.js
+++ app/routes/contact-list.js
@@ -2,6 +2,30 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return this.get('store').findAll('contact');
+        return [{
+          id: 'grand-old-mansion',
+          fn: 'Grand Old Mansion',
+          ln: 'Veruca Salt',
+          email: 'San Francisco',
+          p: 'Estate',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+          bio: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
+        }, {
+          id: 'urban-living',
+          fn: 'Urban Living',
+          ln: 'Mike TV',
+          email: 'Seattle',
+          p: 'Condo',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+          bio: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
+        }, {
+          id: 'downtown-charm',
+          fn: 'Downtown Charm',
+          ln: 'Violet Beauregarde',
+          email: 'Portland',
+          p: 'Apartment',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+          bio: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet.'
+        }];
       }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,7 @@
 export default function() {
   this.namespace = '/api';
 
-  this.get('/contacts', function() {
+  this.get('/contact-list', function() {
     return {
       data: [{
         type: 'contact-list',
