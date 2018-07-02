ced83338a24c7fe6831291a21241f6dec930e1d3
Jonathan Wolper <jpwolper@gmail.com>
2018-07-02 13:37:24 -0400

    first commit

---
+++ tests/acceptance/contact-list-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | contact list', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show Contact List as the home page', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/contact-list', 'should redirect automatically');
+  });
+  test('should list 3 contacts', async function (assert) {  await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 4, 'should display 3 listings');
+  });
+  test('should link to New Contact route', async function (assert) {
+    await visit('/');
+    await click(".menu-new-contact");
+    assert.equal(currentURL(), '/new-contact', 'should navigate to New Contact');
+  });
+});
